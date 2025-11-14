bbf3991baa6882d4f0c6d764eefb363a0d591b7b
Cosmosmedic <110755014+Cosmosmedic@users.noreply.github.com>
2025-11-15 08:08:05 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+EMBED_MODEL = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+GPT_MODEL = "gpt-4o-mini"
 
